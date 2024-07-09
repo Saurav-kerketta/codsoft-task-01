6a35d462d3f042b041cb350df3039ac6a0aaf215
Saurav kerketta <sauravkerketta674@gmail.com>
2024-07-09 23:44:43 +0530

    NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,55 +1,77 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class NumberGame {
+public class QuizApplication {
+    // Quiz question class
+    static class QuizQuestion {
+        String question;
+        String[] options;
+        char correctAnswer;
+
+        public QuizQuestion(String question, String[] options, char correctAnswer) {
+            this.question = question;
+            this.options = options;
+            this.correctAnswer = correctAnswer;
+        }
+    }
+
+    // Define quiz questions
+    static QuizQuestion[] questions = {
+        new QuizQuestion("What is the capital of France?", new String[] {"A. Paris", "B. London", "C. Rome", "D. Madrid"}, 'A'),
+        new QuizQuestion("Who wrote 'Romeo and Juliet'?", new String[] {"A. William Shakespeare", "B. Charles Dickens", "C. Mark Twain", "D. J.K. Rowling"}, 'A'),
+        new QuizQuestion("What is the largest planet in our solar system?", new String[] {"A. Earth", "B. Mars", "C. Jupiter", "D. Saturn"}, 'C')
+    };
+
+    static int score = 0;
+    static int currentQuestionIndex = 0;
+    static boolean answerSubmitted = false;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
-        int minRange = 1;
-        int maxRange = 100;
-        int numberOfAttempts = 10;
-        boolean playAgain = true;
-        int score = 0;
-
-        while (playAgain) {
-            int generatedNumber = random.nextInt(maxRange - minRange + 1) + minRange;
-            int attemptsLeft = numberOfAttempts;
-
-            System.out.println("Welcome to the Number Game!");
-            System.out.println("I have generated a random number between " + minRange + " and " + maxRange + ".");
-            System.out.println("You have " + attemptsLeft + " attempts to guess the number.");
-
-            while (attemptsLeft > 0) {
-                System.out.print("Enter your guess: ");
-                int userGuess = scanner.nextInt();
-
-                if (userGuess == generatedNumber) {
-                    System.out.println("Congratulations! You guessed the correct number.");
-                    score++;
-                    break;
-                } else if (userGuess < generatedNumber) {
-                    System.out.println("Too low! Try again.");
-                } else {
-                    System.out.println("Too high! Try again.");
+        for (QuizQuestion question : questions) {
+            answerSubmitted = false;
+            System.out.println("You have 10 seconds to answer each question.");
+            displayQuestion(question);
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (!answerSubmitted) {
+                        System.out.println("Time's up!");
+                        currentQuestionIndex++;
+                    }
                 }
+            }, 10000);
 
-                attemptsLeft--;
+            char userAnswer = scanner.next().charAt(0);
+            answerSubmitted = true;
+            timer.cancel();
+
+            if (userAnswer == question.correctAnswer) {
+                score++;
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Incorrect! The correct answer was: " + question.correctAnswer);
             }
-
-            if (attemptsLeft == 0) {
-                System.out.println("Sorry, you ran out of attempts. The correct number was " + generatedNumber + ".");
-            }
-
-            System.out.println("Your score is: " + score);
-            System.out.print("Do you want to play again? (Y/N): ");
-            String playAgainInput = scanner.next();
-
-            if (!playAgainInput.equalsIgnoreCase("Y")) {
-                playAgain = false;
-            }
+            currentQuestionIndex++;
         }
 
-        System.out.println("Thank you for playing!");
+        displayResults();
+        scanner.close();
+    }
+
+    static void displayQuestion(QuizQuestion question) {
+        System.out.println(question.question);
+        for (String option : question.options) {
+            System.out.println(option);
+        }
+        System.out.print("Enter your answer (A, B, C, or D): ");
+    }
+
+    static void displayResults() {
+        System.out.println("\nQuiz Over!");
+        System.out.println("Your final score is: " + score + " out of " + questions.length);
     }
 }
